925c6fdbcf7c76b0823896c0d07767fb5655a6b9
Mani Shankar Janumpalli <91050146+Manishankar0801@users.noreply.github.com>
2025-06-24 23:56:10 -0700

    Time: 0 ms (100.00%) | Memory: 55.6 MB (34.31%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,18 +6,22 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let nums1Copy = nums1.slice(0, m)
+    let p1 = m - 1; // Pointer for the last element in nums1
+  let p2 = n - 1; // Pointer for the last element in nums2
 
-    let p1 = 0
-    let p2 = 0
-
-    for (let i=0;i<m+n;i++) {
-        if ( p2 >=n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
-            nums1[i] = nums1Copy[p1]
-            p1++
-        } else {
-            nums1[i] = nums2[p2]
-            p2++
-        }
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (p2 < 0) {
+      break; // If p2 is out of bounds, we are done we can break the loop
     }
+    // but if p1 breaks the condition then we can take nums2[p2] and put it in nums1[i]
+    // p1 >= 0 ensures we don't go out of bounds of nums1
+    // else it will go to else
+    if (p1>=0 && nums1[p1] > nums2[p2]) {
+      nums1[i] = nums1[p1];
+      p1--;
+    } else {
+      nums1[i] = nums2[p2];
+      p2--;
+    }
+  }
 };
