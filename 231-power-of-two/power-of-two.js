f9ae53c444a52fef9306e834169da9b6ccc08ea4
Mani Shankar Janumpalli <91050146+Manishankar0801@users.noreply.github.com>
2025-07-10 00:24:13 -0700

    Time: 0 ms (100.00%) | Memory: 55.2 MB (96.45%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,7 +3,5 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if (n === 1) return true
-    else if ((n%2 !== 0) || n < 1) return false
-    return isPowerOfTwo(n/2)
+    return n > 0 && (n & (n-1)) === 0
 };
